9299f748b3677821c0612530559ff4418601c31c
Oguntoye Opeyemi <ope.oguntoye@protonmail.com>
2019-04-16 07:52:10 +0100

    chore (setup): create react app with webpack
    -setup webpack
    -setup babel
    -setup eslint
    -setup react
    -add heroku generated app.json
    
    [finishes #164010120]

---
+++ config/webpack-modules.js
@@ -0,0 +1,118 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const postCssPresetEnv = require('postcss-preset-env');
+const cssNano = require('cssnano');
+
+exports.devServer = () => ({
+  devServer: {
+    contentBase: './static',
+    historyApiFallback: true,
+    hot: true,
+    open: true,
+    overlay: true,
+    stats: 'errors-only',
+  },
+});
+
+exports.loadHtml = () => ({
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: ['html-loader'],
+      },
+    ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+});
+
+exports.loadJavascript = ({ include } = {}) => ({
+  module: {
+    rules: [
+      {
+        include,
+        test: /\.(js|jsx)$/,
+        use: [
+          'babel-loader',
+          {
+            loader: 'eslint-loader',
+            options: {
+              useEslintrc: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+});
+
+exports.loadStyles = ({ include } = {}) => ({
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        include,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+});
+
+exports.extractStyles = ({ include } = {}) => ({
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        include,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: [
+                postCssPresetEnv({ autoprefixer: { grid: true } }),
+                cssNano({
+                  preset: [
+                    'default',
+                    {
+                      discardComments: { removeAll: true },
+                      cssDeclarationSorter: { order: 'smacss' },
+                    },
+                  ],
+                }),
+              ],
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].[chunkhash:8].css',
+    }),
+  ],
+});
+
+exports.loadImages = ({ include, options } = {}) => ({
+  module: {
+    rules: [
+      {
+        test: /(.jpg|.jpeg|.svg|.png)$/,
+        include,
+        use: [
+          {
+            loader: 'url-loader',
+            options,
+          },
+        ],
+      },
+    ],
+  },
+});
